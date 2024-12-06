c7b34e0d49b806cc85960970ac78ba8be813d6a4
Egito Filho <egitofilho4@gmail.com>
2024-12-06 19:57:31 -0300

    02_23_CONTROLANDO_A_NAVEGACAO_ENTRE_DIFERENTES_TELAS

--- app/src/main/java/com/dev/bernardoslailati/fundamentosandroidapp/SecondFragment.kt
+++ app/src/main/java/com/dev/bernardoslailati/fundamentosandroidapp/SecondFragment.kt
@@ -1,19 +1,32 @@
 package com.dev.bernardoslailati.fundamentosandroidapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.dev.bernardoslailati.fundamentosandroidapp.databinding.FragmentSecondBinding
 
 
 class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val firstArgument = arguments?.getStringArray("first_arg") ?: arrayOf()
+
+        Log.d("SecondFragment", "Argument: ${firstArgument.joinToString()}")
     }
 }
